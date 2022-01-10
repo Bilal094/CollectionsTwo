e63a836f1a6400c614d619d8fa91200ce39b91ca
Bilal094 <99066318@mydavinci.nl>
2022-01-10 14:32:22 +0100

    generator genereert 24 karakters, alleen nog if statement toevoegen

--- password.py
+++ password.py
@@ -1,35 +1,50 @@
-import random
+import random 
+from random import choice, shuffle
 # Assignments
-Hoofdletter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-Kleineletter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-Cijfer = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+Hoofdletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Kleineletter = "abcdefghijklmnopqrstuvwxyz"
+Cijfer = "1234567890"
 Symbolen = ["@", "#", "$", "%", "&", "_", "?"]
-Wachtwoord = []
-HoofdIndex = random.randrange(2,6)
-KleinIndex = random.randrange(8)
-CijferIndex = random.randrange(4,7)
-SymbolenIndex = random.randint(3)
 
-# Code start
-# for m in range(1):
-for x in range(HoofdIndex):
-    x = random.choice(Hoofdletter)
-    Wachtwoord.append(x)
-    
-for y in range(KleinIndex):
-    y = random.choice(Kleineletter)
-    Wachtwoord.append(y)
-
-for z in range(CijferIndex):
-    z = random.choice(Cijfer)
-    Wachtwoord.append(z)
-
-for i in range(SymbolenIndex):
-    i = random.choice(Symbolen)
-    Wachtwoord.append(i)
+def generator():
+    a = random.randint(2,6)
+    b = random.randint(4,7)
+    Wachtwoord = []
 
 
+    for x in range(a):
+        Hoofd = choice(Hoofdletter)
+        Wachtwoord.append(Hoofd)
 
-random.shuffle(Wachtwoord)
-print(Wachtwoord)
+    for y in range(b):
+        Getal = choice(Cijfer)
+        Wachtwoord.append(Getal)
+
+    for z in range(3):
+        Symbool = choice(Symbolen)
+        Wachtwoord.append(Symbool)
+
+    i = x + y + 5
+    lengte = 24 - i
+
+    for k in range(lengte):
+        Klein = choice(Kleineletter)
+        Wachtwoord.append(Klein)
+    return Wachtwoord
+
+
+W = generator()
+shuffle(W)
+if W[0] and W[-1] in Symbolen:
+    while W[0] and W[-1] in Symbolen:
+        shuffle(W)
+        print(W)
+        break
+else:
+    print(W)
+
+
+
+
+
 
