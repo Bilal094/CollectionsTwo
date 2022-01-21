ddbb376e542378acffada80b71c0828c7ac5995a
Bilal094 <99066318@mydavinci.nl>
2022-01-21 11:59:59 +0100

    meer functions gebouwd en verder gebouwd

--- dobbeltrobbel.py
+++ dobbeltrobbel.py
@@ -7,25 +7,48 @@ RoodDobbelsteen = [1, 2, 3, 4, 5, 6]
 WitDobbelsteen = [1, 1, 1, 2, 2, 3]
 RoodMin2 = -2
 BlauwMin2 = -2
-RoodScoreBlad = [RoodMin2, " ", " ", " ", " ", " ", " ", " ", " ", " "]
-BlauwScoreBlad = [" ", " ", " ", " ", " ", " ", " ", " ", " ", BlauwMin2]
-WitScoreBlad = [" ", " ", " ", " ", " "]
-Herhalen = 0
+RoodScoreBladList = [RoodMin2, " ", " ", " ", " ", " ", " ", " ", " ", " "]
+BlauwScoreBladList = [" ", " ", " ", " ", " ", " ", " ", " ", " ", BlauwMin2]
+WitScoreBladList = [" ", " ", " ", " ", " "]
+ScoreBladPositieListRood = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ScoreBladPositieListBlauw = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 Ronde = 0
 maximumEnMinimum = []
 ScoreRood = ''
 ScoreBlauw = ''
 ScoreWit = ''
+ScorePositieBlauw = ''
+ScorePositieRood = ''
 
-for x in RoodScoreBlad:
+for x in RoodScoreBladList:
     ScoreRood += '['+str(x)+'] ' 
-for y in BlauwScoreBlad:
+for y in BlauwScoreBladList:
     ScoreBlauw += '['+str(y)+'] '
-for z in WitScoreBlad:
+for z in WitScoreBladList:
     ScoreWit += '['+str(z)+'] '
+for a in ScoreBladPositieListBlauw:
+    ScorePositieBlauw += ' '+str(a)+'  '
+for a in ScoreBladPositieListRood:
+    ScorePositieRood += ' '+str(a)+'  '
+
     
 def error():
     print('Type a.u.b een gegeven keuze')
+
+def waardenIdentiek(keuze):
+        print(f'Je hebt gekozen voor het getal {keuze}')
+        sleep(1)
+        print('De gerolde waarden van de rode en de blauwe dobbelsteen zijn gelijk aan elkaar')
+        sleep(1)
+        print('Je mag kiezen in welk scoreblad je jouw getal wilt invullen')
+
+def berekeningAenB():
+    if maximumEnMinimum[0] < maximumEnMinimum[1]:
+        print(f'De rode dobbelsteen ({RoodNummer}) heeft het laagst gerolde waarde, dus vul je getal in de rode scoreblad!')
+    elif maximumEnMinimum[1] < maximumEnMinimum[0]:
+        print(f'De blauwe dobbelsteen ({BlauwNummer}) heeft het laagst gerolde waarde, dus vul je getal in de blauwe scoreblad!')
+    else:
+        error()
 
 def dobbelVraag():
     global RoodNummer, BlauwNummer, WitNummer
@@ -46,9 +69,22 @@ def dobbelVraag():
 
 def scoreBlad():
     print('------------------------ Score blad ------------------------')
-    print('Rood = '+ ScoreRood)
-    print('Blauw = '+ ScoreBlauw)
+    print('Rood =  '+ ScoreRood)
+    print('Positie =    '+ ScorePositieRood)
+    print('Blauw =   '+ ScoreBlauw)
+    print('Positie = '+ ScorePositieBlauw)
     print('Wit = ' + ScoreWit)
+
+def positieKeuze(keuze):
+    global ScoreKeuze
+    print(f'Je hebt gekozen voor het getal {keuze}')
+    sleep(1)
+    maximumEnMinimum.pop(2)
+    berekeningAenB()
+    sleep(1.5)
+    scoreBlad()
+    ScoreKeuze = input('Type het nummer waar jij je getal in wilt vullen ')
+       
 
 # Code start ---
 dobbelVraag()
@@ -68,7 +104,26 @@ print(f'A) Blauw + rood + wit = {BlauwNummer} + {RoodNummer} + {WitNummer} = {To
 print(f'B) Blauw + rood - wit = {BlauwNummer} + {RoodNummer} - {WitNummer} = {TotaalB}')
 print(f'C) Blauw + rood = {BlauwNummer} + {RoodNummer} = {TotaalC}')
 print(f'D) Hoogst gerolde dobbelsteen - laagst gerolde dobbelsteen = {maxValue} - {minValue} = {TotaalD}')
+
 berekeningKeuze = input('').upper()
 if berekeningKeuze == 'A':
-    maximumEnMinimum.pop(2)
-    if 
+
+    if maximumEnMinimum[0] == maximumEnMinimum[1]:
+        waardenIdentiek(TotaalA)
+    else:
+        if maximumEnMinimum[0] > maximumEnMinimum[1]:
+            positieKeuze(TotaalA)
+        elif maximumEnMinimum[1] > maximumEnMinimum[0]:
+            positieKeuze(TotaalA)
+            
+elif berekeningKeuze == 'B':
+    
+    if maximumEnMinimum[0] == maximumEnMinimum[1]:
+        waardenIdentiek(TotaalB)
+    else:
+        if maximumEnMinimum[0] > maximumEnMinimum[1]:
+            positieKeuze(TotaalB)
+        elif maximumEnMinimum[1] > maximumEnMinimum[0]:
+            positieKeuze(TotaalB)
+else:
+    error()
