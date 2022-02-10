f3ddccf3d2c715a5a60e6659ea590d9bd69b463c
Bilal094 <99066318@mydavinci.nl>
2022-02-10 22:28:03 +0100

    functies toegevoegd en verder uitgebreid

--- dobbeltrobbel.py
+++ dobbeltrobbel.py
@@ -12,21 +12,30 @@ whiteDice = [1, 1, 1, 2, 2, 3]
 redScore = [Red2, '', '', '', '', '', '', '', '', '']
 blueScore = ['', '', '', '', '', '', '', '', '', Blue2]
 whiteScore = ['','','','','']
+highAndLow = []
 # Functions ---
+
 def positionError():
     print('De gekozen positie is niet geldig')
+
 def error():
     print('Type a.u.b een gegeven keuze')
 
 def numberComparison(red, blue):
     if red == blue:
         return 'even'
-    elif red > blue:
-        return 'blue'
     elif red < blue:
+        return 'blue'
+    elif red > blue:
         return 'red'
 
-
+def evenNumbers():
+    print('De rode en blauwe dobbelstenen hebben dezelfde waarden')
+    sleep(1)
+    print('Je mag kiezen in welke van de 2 lijsten je kunt invullen')
+    print()
+    print(f'A) Rood ({redScore})')
+    print(f'B) Blauw ({blueScore})')
 
 def checkPositionRight(number, position, List):
     global isValidR, isValidL
@@ -50,6 +59,9 @@ def checkPositionRight(number, position, List):
 
     if List[int(position)] == '':
         index = int(position)
+        if index == 9:
+            isValidR = True
+            return isValidR
         while index < len(List) - 1:
             index += 1
             if List[index] != '':
@@ -59,7 +71,6 @@ def checkPositionRight(number, position, List):
             elif index == len(List) - 1:
                 isValidR = True
     return isValidR
-
 
 def checkPositionLeft(number, position, List):
     isValidL = False
@@ -92,49 +103,48 @@ def checkPositionLeft(number, position, List):
                 isValidL = True
     return isValidL
 
-def QuestionAandB(total, option):
+def numberPlacement(total, List):
+        positionBool = True
+        while positionBool:
+            positionChoice = input('Kies waar jij je nummer wilt zetten (0 - 9) of type \'end\' wanneer je jouw nummer nergens in kunt vullen ')
+            if positionChoice == 'end':
+                break
+            pCR = checkPositionRight(total, positionChoice, List)
+            pCL = checkPositionLeft(total, positionChoice, List)
+            if pCR == True and pCL == True:
+                List[int(positionChoice)] = total
+                positionBool = False
+            elif pCR == False and pCL == False:
+                positionError()
 
+def Main(total, option):
     print(f'Je hebt gekozen voor optie {option} ({total})')
     sleep(1)
     nC = numberComparison(redNumber, blueNumber)
     if nC == 'red':
-        print('De rode dobbelsteen heeft het laagst gerold')
+        print('De rode dobbelsteen heeft het hoogst gerold')
         sleep(1)
         print(redScore)
-
-        positionBoolR = True
-        while positionBoolR:
-            positionChoice = input('Kies waar jij je nummer wilt zetten (0 - 9) of type \'end\' wanneer je jouw nummer nergens in kunt vullen ')
-            if positionChoice == 'end':
-                break
-
-            pCR = checkPositionRight(totalA, positionChoice, redScore)
-            pCL = checkPositionLeft(totalA, positionChoice, redScore)
-            if pCR == True and pCL == True:
-                redScore[int(positionChoice)] = totalA
-                positionBoolR = False
-            elif pCR == False and pCL == False:
-                positionError()
+        numberPlacement(total, redScore)
 
     elif nC == 'blue':
-        print('De blauwe dobbelsteen heeft het laagst gerold')
+        print('De blauwe dobbelsteen heeft het hoogst gerold')
         sleep(1)
         print(blueScore)
+        numberPlacement(total, blueScore)
 
-        positionBoolB = True
-        while positionBoolB:
-            positionChoice = input('Kies waar jij je nummer wilt zetten (0 - 9) (Omgekeerd!) of type \'end\' wanneer je jouw nummer nergens in kunt vullen ')
-            if positionChoice == 'end':
-                break
+    elif nC == 'even':
+        evenNumbers()
+        scoreChoice = input('').upper()
+        if scoreChoice == 'A':
+            print(redScore)
+            numberPlacement(total, redScore)
+        elif scoreChoice == 'B':
+            print(blueScore)
+            numberPlacement(total, blueScore)
 
-            pCR = checkPositionRight(totalA, positionChoice, blueScore)
-            pCL = checkPositionLeft(totalA, positionChoice, blueScore)
-            if pCR == True and pCL == True:
-                blueScore[int(positionChoice)] = totalA
-                positionBoolB = False
-            elif pCR == False and pCL == False:
-                positionError()
 # Code start ---
+
 while game < 10:
     while True:
         question1 = input('Type \'1\' om te dobbelen ')
@@ -150,14 +160,34 @@ while game < 10:
 
     totalA = redNumber + blueNumber + whiteNumber
     totalB = redNumber + blueNumber - whiteNumber
+    totalC = redNumber + blueNumber
+    highAndLow.append(redNumber)
+    highAndLow.append(blueNumber)
+    highestNumber = max(highAndLow)
+    lowestNumber = min(highAndLow)
+    totalD = highestNumber - lowestNumber
 
     print(f'A) {redNumber} + {blueNumber} + {whiteNumber} = {totalA}')
     print(f'B) {redNumber} + {blueNumber} - {whiteNumber} = {totalB}')
+    print(f'C) {redNumber} + {blueNumber} = {totalC}')
+    print(f'D) {highestNumber} - {lowestNumber} = {totalD}')
 
     print()
-    numberChoice = input('').upper()
-    if numberChoice == 'A':
-        QuestionAandB(totalA, 'A')
-    elif numberChoice == 'B':
-        QuestionAandB(totalB, 'B')
+    while True:
+        numberChoice = input('').upper()
+        if numberChoice == 'A':
+            Main(totalA, 'A')
+            break
+        elif numberChoice == 'B':
+            Main(totalB, 'B')
+            break
+        elif numberChoice == 'C':
+            Main(totalC, 'C')
+            break
+        elif numberChoice == 'D':
+            Main(totalD, 'D')
+            break
+        else:
+            error()
+
     game += 1
