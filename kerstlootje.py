7a5da5e3c96a2b1f5b521be3d9bcdc8b46d2a8e1
Bilal094 <99066318@mydavinci.nl>
2022-02-18 11:58:44 +0100

    nog 1 bug fixen

--- kerstlootje.py
+++ kerstlootje.py
@@ -1,7 +1,10 @@
 import random
+from random import shuffle, choice
 namesWithDuplicates = []
+namesparticipants = []
 names = []
 main = True
+lootLoop = True
 
 while True:
     question = input('Type de namen van de deelnemers. Type \'1\' als je klaar bent. Type \'stop\' als je wilt stoppen ').lower()
@@ -14,22 +17,30 @@ while True:
         namesWithDuplicates.remove('1')
 
     if question == '1':
-        if len(names) > 1:
+        if len(namesparticipants) > 1:
             lootLoop = True
-            while lootLoop:
-                loot1 = random.choice(names)
-                loot2 = random.choice(names)
-                if loot1 != loot2:
-                    print(f'{loot1} voor {loot2}')
-                    main = False
-                    lootLoop = False
+            break
         else:
             print('Je moet meer dan 1 deelnemer toevoegen!')
 
     for x in namesWithDuplicates:
-        if x not in names:
-            names.append(x)
+        if x not in namesparticipants:
+            namesparticipants.append(x)
             namesWithDuplicates.clear()
         else:
             print('Deze deelnemer is al opgegeven!')
             namesWithDuplicates.clear()
+
+if lootLoop == True:
+    names = [*namesparticipants]
+    while lootLoop:
+        shuffle(namesparticipants)
+        while True:
+            loot1 = choice(namesparticipants)
+            loot2 = choice(names)
+            if loot1 != loot2:
+                namesparticipants.remove(loot1)
+                names.remove(loot2)
+                print(f'{loot1} heeft een lootje van {loot2}')
+                break
+            break
